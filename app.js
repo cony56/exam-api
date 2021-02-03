51834ef543589955f30f63b5edb8983183dfb8c0
cony56 <taeyup56@hanmail.net>
2021-02-03 17:38:00 +0900

    upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 //http://127.0.0.1:3000/
 app.get('/', (req,res) => res.send('Hello NodeJS!'));
 app.get('/service', (req,res) => {
-    var status_name =req.param('status');
+    var status_name =req.param('name');
     res.send('The system is running [Status:' + status_name + ']');
 });
 app.listen(port, () => console.log('Example app listening on port 3000'))
